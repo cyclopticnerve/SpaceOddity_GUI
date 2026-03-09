0ded2d28f57162dfb26fb4ec3f96c1fb43691939
cyclopticnerve <46582023+cyclopticnerve@users.noreply.github.com>
2026-03-09 16:31:29 -0400

    new file

---
+++ src/gui/python/keys.py
@@ -0,0 +1,79 @@
+# ------------------------------------------------------------------------------
+# Project : SpaceOddity_GUI                                        /          \
+# Filename: keys.py                                               |     ()     |
+# Date    : 12/29/2025                                            |            |
+# Author  : cyclopticnerve                                        |   \____/   |
+# License : WTFPLv2                                                \          /
+# ------------------------------------------------------------------------------
+
+"""
+A module containing common keys as strings
+"""
+
+# general keys
+S_KEY_CRON = "cron"
+S_KEY_CRON_ENABLED = "cron_enabled"
+S_KEY_CRON_INTERVAL = "cron_interval"
+
+# caption keys
+S_KEY_CAPTION = "caption"
+S_KEY_CAPTION_SHOW = "caption_show"
+S_KEY_CAPTION_POS = "caption_pos"
+S_KEY_CAPTION_WRAP = "caption_wrap"
+
+# what to show
+S_KEY_CAPTION_INFO = "caption_info"
+S_KEY_CAPTION_INFO_TITLE = "caption_info_title"
+S_KEY_CAPTION_INFO_DATE = "caption_info_date"
+S_KEY_CAPTION_INFO_COPY = "caption_info_copy"
+S_KEY_CAPTION_INFO_EXP = "caption_info_exp"
+
+# font stuff
+S_KEY_CAPTION_FONT = "caption_font"
+S_KEY_CAPTION_FONT_NAME = "caption_font_name"
+S_KEY_CAPTION_FONT_SIZE = "caption_font_size"
+S_KEY_CAPTION_FONT_COLOR = "caption_font_color"
+S_KEY_CAPTION_FONT_TRANS = "caption_font_trans"
+
+# box stuff
+S_KEY_CAPTION_BOX = "caption_box"
+S_KEY_CAPTION_BOX_COLOR = "caption_box_color"
+S_KEY_CAPTION_BOX_TRANS = "caption_box_trans"
+S_KEY_CAPTION_BOX_RAD = "caption_box_rad"
+S_KEY_CAPTION_BOX_PAD = "caption_box_pad"
+
+# padding
+S_KEY_CAPTION_PAD = "caption_pad"
+S_KEY_CAPTION_PAD_L = "caption_pad_l"
+S_KEY_CAPTION_PAD_T = "caption_pad_t"
+S_KEY_CAPTION_PAD_R = "caption_pad_r"
+S_KEY_CAPTION_PAD_B = "caption_pad_b"
+
+# positions
+S_KEY_CAP_POS_TL = 0
+S_KEY_CAP_POS_TC = 1
+S_KEY_CAP_POS_TR = 2
+S_KEY_CAP_POS_CL = 3
+S_KEY_CAP_POS_CC = 4
+S_KEY_CAP_POS_CR = 5
+S_KEY_CAP_POS_BL = 6
+S_KEY_CAP_POS_BC = 7
+S_KEY_CAP_POS_BR = 8
+
+# apod keys
+S_KEY_APOD = "apod"
+S_KEY_APOD_TYPE = "media_type"
+S_KEY_APOD_HDURL = "hdurl"
+S_KEY_APOD_URL = "url"
+S_KEY_APOD_TITLE = "title"
+S_KEY_APOD_DATE = "date"
+S_KEY_APOD_COPY = "copyright"
+S_KEY_APOD_EXP = "explanation"
+S_KEY_APOD_VER = "service version"
+
+# screen keys
+S_KEY_SCREEN = "screen"
+S_KEY_SCREEN_WIDTH = "screen_width"
+S_KEY_SCREEN_HEIGHT = "screen_height"
+
+# -)
