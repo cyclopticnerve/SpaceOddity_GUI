d24fa8478e4dbe2cec0749179b5a45fab2a6db04
cyclopticnerve <46582023+cyclopticnerve@users.noreply.github.com>
2026-03-17 21:42:49 -0400

    remove unneeded code

--- src/spaceoddity_gui.py
+++ src/spaceoddity_gui.py
@@ -74,34 +74,6 @@ class SpaceoddityGui(SpaceoddityGuiBase):
     # Constants
     # --------------------------------------------------------------------------
 
-
-    # cmd line options
-
-    # enable option strings
-    # S_ARG_X_OPTION = "-x"
-    # S_ARG_X_ACTION = "store_true"
-    # S_ARG_X_DEST = "X_DEST"
-    # # I18N: enable mode help
-    # S_ARG_X_HELP = _("do x")
-
-    # # --------------------------------------------------------------------------
-    # # Initialize the new object
-    # # --------------------------------------------------------------------------
-    # def __init__(self):
-    #     """
-    #     Initialize the new object
-
-    #     Initializes a new instance of the class, setting the default values
-    #     of its properties, and any other code that needs to run to create a
-    #     new object.
-    #     """
-
-    #     # do super init
-    #     super().__init__()
-
-    #     # NB: add class properties here
-    #     self._foo = True
-
     # --------------------------------------------------------------------------
     # Public methods
     # --------------------------------------------------------------------------
@@ -122,9 +94,6 @@ class SpaceoddityGui(SpaceoddityGuiBase):
         # then add common options (from super)
         self._setup()
 
-        # do something with -x option
-        # if self._dict_args[self.S_ARG_X_DEST]:
-        #     pass
         # ----------------------------------------------------------------------
         # main stuff
 
@@ -137,101 +106,6 @@ class SpaceoddityGui(SpaceoddityGuiBase):
 
         # call boilerplate code
         self._teardown()
-
-    # --------------------------------------------------------------------------
-    # Private methods
-    # --------------------------------------------------------------------------
-
-    # NB: these are the main steps, called in order from main
-
-    # # --------------------------------------------------------------------------
-    # # Boilerplate to use at the start of main
-    # # --------------------------------------------------------------------------
-    # def _setup(self):
-    #     """
-    #     Boilerplate to use at the start of main
-
-    #     Perform some mundane stuff like setting properties.
-    #     If you implement this function. make sure to call super() LAST!!!
-    #     """
-
-        # # add debug option
-        # self._parser.add_argument(
-        #     self.S_ARG_DBG_OPTION,
-        #     action=self.S_ARG_DBG_ACTION,
-        #     dest=self.S_ARG_DBG_DEST,
-        #     help=self.S_ARG_DBG_HELP,
-        # )
-
-        # # add config option
-        # self._parser.add_argument(
-        #     self.S_ARG_CFG_OPTION,
-        #     dest=self.S_ARG_CFG_DEST,
-        #     help=self.S_ARG_CFG_HELP,
-        #     metavar=self.S_ARG_CFG_METAVAR
-        # )
-
-        # add x option
-        # self._parser.add_argument(
-        #     self.S_ARG_X_OPTION,
-        #     action=self.S_ARG_X_ACTION,
-        #     dest=self.S_ARG_X_DEST,
-        #     help=self.S_ARG_X_HELP,
-        # )
-
-        # NB: do setup last
-        # super()._setup()
-
-        # NB: self._dict_args are now available
-        # as well as self._dict_cfg
-
-    # # --------------------------------------------------------------------------
-    # # Short description
-    # # --------------------------------------------------------------------------
-    # def _func(self):
-    #     """
-    #     Short description
-
-    #     Args:
-    #         var_name: Short description
-
-    #     Returns:
-    #         Description
-
-    #     Raises:
-    #         exception_type(vars): Description
-
-    #     Long description (including HTML).
-    #     """
-
-    #     # self._logger.info("func")
-
-    #     # debug_foo
-    #     if self._arg_debug:
-    #         # I18N: context for this string
-    #         return _("this is func (DEBUG)")
-
-    #     # no debug, return normal result
-    #     # I18N: context for this string
-    #     return _("this is func")
-
-    # # --------------------------------------------------------------------------
-    # # Boilerplate to use at the end of main
-    # # --------------------------------------------------------------------------
-    # def _teardown(self):
-    #     """
-    #     Boilerplate to use at the end of main
-
-    #     Perform some mundane stuff like saving properties.
-    #     If you implement this function. make sure to call super() LAST!!!
-    #     """
-
-    #     # save a prop
-    #     self._dict_cfg["foo"] = "bar"
-
-    #     # do teardown
-    #     super()._teardown()
-
 
 # ------------------------------------------------------------------------------
 # Code to run when called from command line
